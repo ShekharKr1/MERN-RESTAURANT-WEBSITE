644df791146d177f107e002238cb227652cc3f63
ShekharKr1 <shekharkr620@gmail.com>
2024-08-23 21:58:40 +0530

    file added

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { dbConnection } from "./database/dbConnection.js";
 import { errorMiddleware } from './error/error.js';
 import reservationRouter from "./routes/reservationRoute.js";
+import path from 'path';
 const app = express();
 dotenv.config({ path: "./config/.env" })
 
@@ -22,4 +23,27 @@ dbConnection();
 app.use(errorMiddleware)
 app.use("/api/v1/reservation", reservationRouter)
 export default app;
+
+
+//-----------------------------------------Deployement----------------------------------------------
+
+const __dirname1 = path.resolve()
+if (process.env.NODE_ENV === "productions") {
+    app.use(express.static(path.join(__dirname1, "../frontend/dist")));
+
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname1, "frontend", "dist", "index.html"));
+    });
+
+
+} else {
+    app.get("/", (req, res) => {
+        res.send("API Running Succesfully......")
+    })
+}
+
+
+//-----------------------------------------Deployement----------------------------------------------
+
 
